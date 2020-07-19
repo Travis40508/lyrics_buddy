793443cffe046e3a145731c3403763818c659921
Travis Tressler <r.travis.tressler@gmail.com>
2020-07-18 20:49:08 -0400

    Cleaned up songs screen and created custom song card

--- lib/ui/songs/songs_screen.dart
+++ lib/ui/songs/songs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:lyrics_buddy/models/state/song_library.dart';
+import 'package:lyrics_buddy/ui/widgets/song_card.dart';
 import 'package:provider/provider.dart';
 
 class SongsScreen extends StatefulWidget {
@@ -28,101 +29,7 @@ class _SongsScreenState extends State<SongsScreen> {
                   itemBuilder: (context, index) {
                     final song = songLibrary.songs[index];
 
-                    return Container(
-                      height: MediaQuery.of(context).size.height / 3,
-                      child: Card(
-                        color: Colors.black,
-                        elevation: 4.0,
-                        child: Stack(
-                          fit: StackFit.passthrough,
-                          children: <Widget>[
-                            Opacity(
-                              opacity: .5,
-                              child: Row(
-                                children: <Widget>[
-                                  Expanded(
-                                    child: Image(
-                                      fit: BoxFit.cover,
-                                      image: CachedNetworkImageProvider(
-                                        song.artistImage
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Image(
-                                      fit: BoxFit.cover,
-                                      image: CachedNetworkImageProvider(
-                                          song.albumImage
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Positioned(
-                              top: 10.0,
-                              left: 5.0,
-                              child: Stack(
-                                fit: StackFit.loose,
-                                children: <Widget>[
-                                  Positioned.fill(
-                                    child: Opacity(
-                                      opacity: .5,
-                                      child: Card(
-                                        elevation: 4.0,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      song.artistName,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 14.0
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ),
-                            Positioned(
-                                bottom: 10.0,
-                                right: 5.0,
-                                child: Stack(
-                                  fit: StackFit.loose,
-                                  children: <Widget>[
-                                    Positioned.fill(
-                                      child: Opacity(
-                                        opacity: .5,
-                                        child: Card(
-                                          elevation: 4.0,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        song.title,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 14.0
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
+                    return SongCard(song);
                   },
                 ),
               );
---
+++ lib/ui/widgets/song_card.dart
@@ -0,0 +1,114 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:lyrics_buddy/models/song.dart';
+
+class SongCard extends StatelessWidget {
+  
+  final Song _song;
+  SongCard(this._song);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      child: Card(
+        color: Colors.black,
+        elevation: 4.0,
+        child: Stack(
+          fit: StackFit.passthrough,
+          children: <Widget>[
+            Opacity(
+              opacity: .6,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Hero(
+                      tag: '${_song.artistName}${_song.title}',
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: CachedNetworkImageProvider(
+                            _song.artistImage
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Hero(
+                      tag: '${_song.title}${_song.artistName} ',
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: CachedNetworkImageProvider(
+                            _song.albumImage
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+                top: 10.0,
+                left: 5.0,
+                child: Stack(
+                  fit: StackFit.loose,
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Opacity(
+                        opacity: .5,
+                        child: Card(
+                          elevation: 4.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _song.artistName,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+            Positioned(
+                bottom: 10.0,
+                right: 5.0,
+                child: Stack(
+                  fit: StackFit.loose,
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Opacity(
+                        opacity: .5,
+                        child: Card(
+                          elevation: 4.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _song.title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
